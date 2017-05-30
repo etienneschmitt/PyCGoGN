4608a91d2f1f3bad365a762963fb2d2378f97c83
Étienne Schmitt <etienne@schmitt.wf>
2017-05-31 00:31:39 +0200

    Modified python example app.
    Signed-off-by: Étienne Schmitt <etienne@schmitt.wf>

--- examples/sandbox.py
+++ examples/sandbox.py
@@ -1,42 +1,56 @@
-import pycgogn
+import pycgogn as pc
 import numpy as np
 import sys
 
-def afficher(x):
-	print(x)
-	if x.dart.index <= 15:
-		return False
-	else:
-		return True
 
-d = pycgogn.Dart(12)
+
+d = pc.Dart(12)
 d.index = 4;
 print(d)
 
-c = pycgogn.CellP1()
+c = pc.CellP1()
 print(c)
 print(c.is_valid())
 
 if len(sys.argv) >= 2:
-	map = pycgogn.CMap2Tri()
+	map = pc.CMap2()
 	mesh_path = sys.argv[1]
-	pycgogn.import_surface_tri(map,mesh_path)
+	pc.import_surface(map,mesh_path)
 	print(map.nb_vertices())
-	map.foreach_vertex_until(afficher)
-	
-	a = map.add_vertex_attribute_vec3d("des_vec3")
 
-	def init_plongement_vec3(x):
-		a[x] = np.array([1.2, 3.5, 5.1])
+	dmarker = pc.DartMarkerStore2(map)
+	vmarker = pc.VertexMarkerStore2(map)
 
-	map.foreach_vertex(init_plongement_vec3)
-	# map.foreach_vertex(lambda x : print(a[x][1]))
-	def afficher_vec3(v):
-		if v.dart.index <= 15:
-			print(a[x][2])
-			return False
-		else:
+	def afficher(c):
+		def afficher_arete(e):
+			print(e)
+		map.foreach_incident_edge(c, afficher_arete)
+		dmarker.mark(c.dart)
+		# vmarker.mark(c)
+		print(c)
+		if c.dart.index <= 8:
 			return True
-	map.foreach_vertex_until(afficher_vec3)
+		else:
+			return False
+	map.foreach_vertex_until(afficher)
+	# dmarker.unmark_all()
+	# print(dmarker.marked_darts())
+	# print(vmarker.marked_cells())
+	a = map.add_vertex_attribute_mat("une_matrice")
 
-print(pycgogn.Orbit.__members__)
+	def init_plongement_vec(x):
+		a[x] = np.array([[1.2, 3.5, 5.1, 5.2, 0.8, -2.1],
+			             [3.6, 5.3, 7.4, 9.5, 0.1, -5.2]])
+
+	map.foreach_vertex(init_plongement_vec)
+	# map.foreach_vertex(lambda x : print(a[x][1]))
+	def afficher_vec3(c):
+		if c.dart.index <= 15:
+			print(c)
+			print(a[c])
+			return True
+		else:
+			return False
+	map.foreach_vertex(afficher_vec3)
+
+print(pc.Orbit.__members__)
